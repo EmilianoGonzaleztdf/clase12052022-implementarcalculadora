2c348f892376ba7192566579ef4070745c40d1b8
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-12 20:06:17 -0300

    funciona me falta hacer loop de (40 -)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-import "./styles.css";
+rotulo1.innerHTML = "ingrese el primer valor";
+rotulo2.innerHTML = "ingrese el segundo valor";
+rotulo3.innerHTML = "ingrese 1 para sumar o 2 para restar";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+let valor1 = document.getElementById("dato1");
+let valor2 = document.getElementById("dato2");
+let valor3 = document.getElementById("dato3");
+
+
+btn1.addEventListener("click", () => {
+  let valor1: number = Number(dato1.value);
+  let valor2: number = Number(dato2.value);
+  let valor3: number = Number(dato3.value);
+
+  console.log("--------------------------------------");
+  if(valor3 == 1){
+    console.log("el resultado es : " valor1 + valor2);
+  }else{(valor3 == 2)
+    console.log("el resultado es :" valor1 - valor2);
+  }
+  console.log("--------------------------------------");
+});
