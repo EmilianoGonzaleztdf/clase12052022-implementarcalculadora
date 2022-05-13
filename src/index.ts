bfe745f9d4df427d26973f6ceeea10afe61ad45f
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-13 00:16:59 -0300

    cambie if/else por SWITCH mejore mi codigo. Falta poder asignar valor a Guiones

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,40 @@
 rotulo1.innerHTML = "ingrese el primer valor";
 rotulo2.innerHTML = "ingrese el segundo valor";
 rotulo3.innerHTML = "ingrese 1 para sumar o 2 para restar";
-rotulo4.innerHTML = "ingrese la cantidad de guiones";
 
 let btn1 = document.getElementById("btn1");
-let n = document.getElementById("dato4.value");
 
 btn1.addEventListener("click", () => {
   let valor1: number = Number(dato1.value);
   let valor2: number = Number(dato2.value);
   let valor3: number = Number(dato3.value);
 
-  function dibujarGuiones(n:number){
+  function dibujarGuiones(){
     let guion: string = "-";
-    for (let i = 0; i <= n; i++) {
+    for (let i = 0; i <= 40; i++) {
       guion = guion + "-";
     }
     console.log(guion);
   };
 
-  if(valor3 == 1){
-    dibujarGuiones(n);
-    console.log("el resultado es : " valor1 + valor2);
-    dibujarGuiones(n);
-  }else if(valor3 == 2){
-    dibujarGuiones(n);
-    console.log("el resultado es :" valor1 - valor2);
-    dibujarGuiones(n);
-  }else{
-    dibujarGuiones(n);
-    console.log("fin del algoritmo");
-    dibujarGuiones(n);
-  }
+  let sumar :number = valor1 + valor2;
+  let restar :number = valor1 - valor2;
+  let operacion:number = valor3;
 
+  switch(operacion){
+    case 1:
+      dibujarGuiones();
+      console.log("el resultado es: " sumar);
+      dibujarGuiones();
+      break;
+    case 2:
+        dibujarGuiones();
+      console.log("el resultado es: " restar);
+      dibujarGuiones();
+      break;
+    default:
+        dibujarGuiones();
+      console.log(" *Fin del Algoritmo* ");
+      dibujarGuiones();
+  }
 });
