c6f654ec070139a872eaeeb61bff60349fc5b87c
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-13 19:29:39 -0300

    agregue cantidad de guiones ingresada x usuario no me imprime la cantidad de guiones que quiero. Es momento de consultar a Jose

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,34 @@
 rotulo1.innerHTML = "ingrese el primer valor";
 rotulo2.innerHTML = "ingrese el segundo valor";
 rotulo3.innerHTML = "ingrese 1 para sumar o 2 para restar";
+rotulo4.innerHTML = "ingrese la cantidad de guiones";
 
 let btn1 = document.getElementById("btn1");
+let dato4 = document.getElementById("dato4");
 
 btn1.addEventListener("click", () => {
   let valor1: number = Number(dato1.value);
   let valor2: number = Number(dato2.value);
   let valor3: number = Number(dato3.value);
-
+  
   function guionesParaTodos(n:number){
     let guion: string = "-";
-    for (let i = 0; i <= n; i++) {
+    for (let i = 0; i <= (n); i++) {
       guion = guion + "-";
-    }
+    };
     
     console.log(guion);
   };
-
+  let n: number = Number(dato4.value);
   let sumar :number = valor1 + valor2;
   let restar :number = valor1 - valor2;
   let operacion:number = valor3;
 
   switch(operacion){
     case 1:
-      guionesParaTodos();
+      guionesParaTodos(n);
       console.log("el resultado es: " sumar);
-      guionesParaTodos();
+      guionesParaTodos(n);
       break;
     case 2:
       guionesParaTodos();
@@ -34,8 +36,8 @@ btn1.addEventListener("click", () => {
       guionesParaTodos();
       break;
     default:
-      guionesParaTodos(1);
+      guionesParaTodos(n);
       console.log(" *Fin del Algoritmo* ");
-      guionesParaTodos(1);
+      guionesParaTodos(n);
   }
 });
