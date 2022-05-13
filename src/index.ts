c204f5e40087d64069ea56791aff31f44a8c9efe
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-12 21:13:58 -0300

    agregue cantidad de guiones alto codigo :D gracias curso

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 rotulo1.innerHTML = "ingrese el primer valor";
 rotulo2.innerHTML = "ingrese el segundo valor";
 rotulo3.innerHTML = "ingrese 1 para sumar o 2 para restar";
-  
+rotulo4.innerHTML = "ingrese la cantidad de guiones";
+
 let btn1 = document.getElementById("btn1");
+let n = document.getElementById("dato4.value");
 
 btn1.addEventListener("click", () => {
   let valor1: number = Number(dato1.value);
@@ -18,17 +20,17 @@ btn1.addEventListener("click", () => {
   };
 
   if(valor3 == 1){
-    dibujarGuiones(20);
+    dibujarGuiones(n);
     console.log("el resultado es : " valor1 + valor2);
-    dibujarGuiones(20);
+    dibujarGuiones(n);
   }else if(valor3 == 2){
-    dibujarGuiones(40);
+    dibujarGuiones(n);
     console.log("el resultado es :" valor1 - valor2);
-    dibujarGuiones(40);
+    dibujarGuiones(n);
   }else{
-    dibujarGuiones(5);
+    dibujarGuiones(n);
     console.log("fin del algoritmo");
-    dibujarGuiones(5);
+    dibujarGuiones(n);
   }
 
 });
